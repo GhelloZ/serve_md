ac536aa3b6ce01212dd25960ea9c7c74603c6278
GhelloZ <gustavozuffellato@icloud.com>
2025-11-10 13:15:33 +0100

    Added parameters for IP address, port and file path

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 use tiny_http::{Server, Response, Header};
 use markdown::to_html;
-user clap::Parser;
+use clap::Parser;
+
+/// A minimal markdown web server
+#[derive(Parser, Debug)]
+#[command(version, about = "Serve a markdown file as HTML")]
+struct Args {
+    /// IP Address to bind to
+    #[arg(short='a', long="address", default_value="0.0.0.0")]
+    address: String,
+
+    /// Port to listen on
+    #[arg(short='p', long="port", default_value_t = 3000)]
+    port: u16,
+
+    /// Path to the markdown file to host
+    #[arg(default_value = "./index.md")]
+    file: PathBuf,
+}
 
 fn main() {
-    let server = Server::http("0.0.0.0:3000").unwrap();
-    let file_path = Path::new("./sample.md");
+    let args = Args::parse();
+    let bind_addr = format!("{}:{}", args.address, args.port);
 
-    println!("Serving markdown page at http://127.0.0.1:3000");
+    println!("Serving {} at http://{} ...", args.file.display(), bind_addr);
+
+    let server = Server::http(&bind_addr).unwrap();
 
     for request in server.incoming_requests(){
-        let markdown = fs::read_to_string(file_path).unwrap_or_else(|_| "# File not found".into());
+        let markdown = fs::read_to_string(&args.file)
+            .unwrap_or_else(|_| "# File not found".into());
         let html_content = to_html(&markdown);
 
         let html_page = format!(
